4c064f92254c514350cbf553aa6e87f79fc62e84
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2021-01-24 12:49:10 -0300

    ASP

---
+++ CrudAspCore/wwwroot/Models/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudAspCore.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 5)]
+        public String Nome { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Nascimento { get; set; }
+
+        [Required]
+        [Range(1, 99999.99)] // Valor Mínimo ao Máximo
+        [DataType(DataType.Currency)]
+        [Display(Name = "Credito")]
+        public Decimal Credito { get; set; }
+    }
+}

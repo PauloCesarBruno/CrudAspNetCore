8b9d898749b1a235cffb9a5d0087dcdfd5642445
Paulo Bruno <p_bruno001@hotmail.com>
2021-05-18 18:17:51 -0300

    ASP

--- CrudAspCore/Controllers/ClienteController.cs
+++ CrudAspCore/Controllers/ClienteController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CrudAspCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using CrudAspCore.Models;
 
 namespace CrudAspCore.Controllers
 {
